3802a443b6d690918cd6a84e12c8e826df0e678d
shayastark <info@shayastark.com>
2026-03-28 22:27:30 -0700

    feat: themes, settings, TTS learning, physics, surprises

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'smash-keys-v1';
+const CACHE_NAME = 'smash-keys-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
